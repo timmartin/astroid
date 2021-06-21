493ddc6b2d0376b5178a7f1d024de079b1a08dfa
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-06-21 10:34:33 +0200

    Add 'Pattern' to the astroid.nodes's API

--- astroid/rebuilder.py
+++ astroid/rebuilder.py
@@ -55,7 +55,7 @@ from astroid import nodes
 from astroid._ast import ParserModule, get_parser_module, parse_function_type_comment
 from astroid.const import PY37_PLUS, PY38_PLUS, PY39_PLUS, Context
 from astroid.manager import AstroidManager
-from astroid.nodes.node_classes import NodeNG
+from astroid.nodes import NodeNG
 
 if TYPE_CHECKING:
     import ast
@@ -479,7 +479,7 @@ class TreeRebuilder:
             ...
 
         @overload
-        def visit(self, node: "ast.pattern", parent: NodeNG) -> node_classes.Pattern:
+        def visit(self, node: "ast.pattern", parent: NodeNG) -> nodes.Pattern:
             ...
 
         @overload
